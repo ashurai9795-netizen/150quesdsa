93450959df35957e291e8349693629123075ed1b
Ashutosh Rai <ashurai9795@gmail.com>
2026-07-03 23:56:25 +0530

    Time: 17 ms (39.79%), Space: 43.9 MB (58.81%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character>map=new HashMap<>();
+         HashMap<Character,Character>map1=new HashMap<>();
+         for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            if(map.containsKey(c1)){
+                if(map.get(c1)!=c2){
+                    return false;
+                }
+                }else{
+                map.put(c1,c2);
+                }
+            
+            if(map1.containsKey(c2)){
+                if(map1.get(c2)!=c1){
+                    return false;
+                }
+            }
+                else{
+                map1.put(c2,c1);
+                }
+            }
+
+         
+         return true;
+
+    }
+}
