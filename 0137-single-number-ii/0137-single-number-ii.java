9cb831c4bcd2e24064b374f3ffabbd282d298e2c
Ashutosh Rai <ashurai9795@gmail.com>
2026-07-09 23:25:52 +0530

    Time: 6 ms (42.13%), Space: 46.8 MB (5.38%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -2,7 +2,11 @@ class Solution {
     public int singleNumber(int[] nums) {
        HashMap<Integer,Integer>map=new HashMap<>();
        for(int num:nums){
-        map.put(num,map.getOrDefault(num,0)+1);
+        if (map.containsKey(num)) {
+            map.put(num, map.get(num) + 1);
+           } else {
+          map.put(num, 1);
+            }
        }
        for(int num:nums){
         if(map.get(num)==1) return num;
